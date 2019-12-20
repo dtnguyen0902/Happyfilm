5300c07ea797e527d4c394bc3f01a31507ad1e46
nguyenstunt <nguyendt0902@gmail.com>
2019-12-20 10:45:45 +0700

    Update boxoffice

--- happyfilm/src/components/About/AboutHappyFilm.js
+++ happyfilm/src/components/About/AboutHappyFilm.js
@@ -3,7 +3,7 @@ import DetailAbout from './DetailAbout';
 class AboutHappyFilm extends Component {
     render() {
         return (
-            <div className='container'>
+            <div className='container' id="aboutPage">
                 <ul className="nav nav-pills container myPromotion" id="pills-tab" role="tablist">
                     <li className="nav-item myPromotion">
                         <a className="nav-link active myPromotion" id="pills-home-tab" data-toggle="pill" role="tab">HAPPY FILM</a>
